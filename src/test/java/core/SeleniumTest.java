8213063ad88712af229903296682170d7ca96b61
Elena Elshina <elshinalena@yahoo.com>
2015-09-11 12:49:46 -0700

    HW41

---
+++ src/test/java/core/SeleniumTest.java
@@ -0,0 +1,47 @@
+package core;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.BeforeClass;
+import org.junit.ComparisonFailure;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+
+public class SeleniumTest {
+
+    @BeforeClass
+    public static void BeforeClass01() throws Exception  {
+    }
+
+    // Test data generator
+    @Parameters(name = "Iteration # {index} : exp: {1}; act: {2}")
+    public static Collection<String[]> data() throws IOException {
+           core.Selenium selen = new core.Selenium();
+           return Arrays.asList(selen.a2d());
+    }
+
+    // @Parameter for Field injection instead of Constructor
+    @Parameter(value = 0)
+    public String test_id;
+
+    @Parameter(value = 1)
+    public String expected_Result;
+
+    @Parameter(value = 2)
+    public String actual_Result;
+    
+    @Test
+    public void test_Title_Validation()  throws ComparisonFailure {
+    System.out.println("Test Case: " + test_id + " Expected Result: " + expected_Result + "  Actual Result: " + actual_Result);
+           assertEquals("  FAILED", expected_Result, actual_Result);
+    }
+
+}
